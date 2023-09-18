567b8af948a41eafb4481eb7ae37d4d1ac534e55
zuhaibkhalifa <zuhaib@mvp-apps.ae>
2023-09-18 19:26:11 +0400

    seo things

---
+++ generate-sitemap.js
@@ -0,0 +1,47 @@
+console.log("Starting sitemap generation...");
+
+const { createWriteStream } = require("fs");
+
+const pages = [
+  "app/locale",
+  "app/coding",
+  "app/coding/c",
+  "app/coding/cplus",
+  "app/coding/csharp",
+  "app/coding/java",
+  "app/coding/python",
+  "app/coding/javascript",
+  "app/coding/html",
+  "app/coding/css",
+  "app/coding/php",
+  "app/coding/ruby",
+  "app/media/art",
+  "app/media/audio",
+  "app/media/enhance",
+  "app/media/redesign",
+  "app/media/remove",
+];
+
+let sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+`;
+
+pages.forEach((page) => {
+  sitemapContent += `
+    <url>
+        <loc>https://www.yourwebsite.com/${page}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.7</priority>
+    </url>
+  `;
+});
+
+sitemapContent += `</urlset>`;
+
+try {
+  createWriteStream("public/sitemap.xml").write(sitemapContent, () => {
+    console.log("Sitemap written successfully!");
+  });
+} catch (error) {
+  console.error("Error generating sitemap:", error);
+}
