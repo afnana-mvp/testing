58b955f325a1744c281148de1128291f979dda98
afnana <afnana@mvp-apps.ae>
2023-09-19 11:29:53 +0400

    m

---
+++ src/app/[locale]/writing/facebook/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Facebook = () => {
+  return (
+    <div className="flex items-center justify-center flex-col gap-10">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Facebook;
