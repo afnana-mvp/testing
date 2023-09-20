0ab307424ac93afb12f399838d8c13e6f699f67a
zuhaibkhalifa <zuhaib@mvp-apps.ae>
2023-09-20 15:57:41 +0400

    languages section

--- src/app/[locale]/coding/html/page.tsx
+++ src/app/[locale]/coding/html/page.tsx
@@ -1,9 +1,6 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Image from "next/image";
-import cSharpImage from "~/coding/csharp_tool/csharp_main.png";
 import { useTranslations } from "next-intl";
-
 import htmlIcon from "~/coding/html/html_icon.png";
 import htmlCode from "~/coding/html/html_code.png";
 import pythonSide from "~/coding/python_tool/python_side.png";
@@ -12,64 +9,54 @@ const HtmlCoding = () => {
   const t = useTranslations("Html");
 
   return (
-    <div className="bg-background_color  flex items-center justify-center flex-col gap-[50px] text-center">
-      <div className="flex items-center w-full justify-center bg-[url('../../public/coding/c/deepLearning_background.png')]  bg-cover bg-center	bg-no-repeat h-[90vh] ">
-        <h1 className="text-white text-center text-xl md:text-5xl font-semibold container  ">
-          {t("htmlimageText")} <span className="araby_ai"> ArabyAi</span>
+    <section className="bg-background_color flex items-center justify-center flex-col gap-[50px] text-center">
+      <header className="flex items-center w-full justify-center bg-[url('../../public/coding/c/deepLearning_background.png')] bg-cover bg-center	bg-no-repeat h-[50vh] lg:h-[90vh]">
+        <h1 className="text-white text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold !leading-relaxed " aria-label="Main Title">
+          {t("htmlimageText")}
+          <br />
+          {t("language by")}
+          <span className="araby_ai"> ArabyAi</span>
         </h1>
-      </div>
-      <div className="flex items-center  justify-center gap-10 flex-col md:flex-row w-5/6 ">
-        <Image src={htmlIcon} alt="python image" width={120} className="" />
-        <p className="text-[12px] md:text-xl basis-9/12	md:text-left rtl:text-right">
+      </header>
+
+      <div className="flex items-center justify-center gap-10 flex-col md:flex-row w-5/6 ">
+        <Image src={htmlIcon} alt="HTML Icon" width={100} className="w-20 md:w-32" />
+        <p className="text-sm md:text-xl basis-9/12	md:text-left rtl:text-right" aria-label="Description about HTML Icon">
           {t("htmliconText")}
         </p>
       </div>
-      <div className="flex items-center justify-center w-5/6 md:w-full">
-        <h1 className="text-center text-xl md:text-3xl font-bold container ">
+
+      <section className="flex items-center justify-center w-5/6 md:w-full">
+        <h2 className="text-center text-xl md:text-3xl font-bold container " aria-label="Subheading">
           {t("htmlheading")}
           <span className="araby_ai"> Araby AI</span>
-        </h1>
-      </div>
-      <div className="flex items-center justify-center  container flex-col md:flex-row  md:w-2/3 gap-10 md:gap-0 ">
-        <p className="mx-8 text-[12px] md:text-xl md:text-left basis-1/2 rtl:text-right">
+        </h2>
+      </section>
+
+      <article className="flex items-center justify-center container flex-col md:flex-row md:w-2/3 gap-10 md:gap-0 ">
+        <p className="mx-8 text-sm md:text-xl md:text-left basis-1/2 rtl:text-right" aria-label="Description about HTML help">
           {t("htmlhelpText")}
         </p>
 
-        <div className="border-gradient-help inline-flex items-center  border-solid border-2 rounded-xl text-gray-100 text-xs md:text-lg gradient-border-3  flex-col  justify-center  gap-5 md:gap-5 p-1   basis-1/2 w-[200px] md:w-auto h-[500px]">
-          <div className="bg-[#1c1c1ccc] w-full  flex justify-center items-center flex-col gap-5  object-contain h-full">
-            <div className="text-sm md:text-[40px] leading-[20px] md:leading-[40px] text-[#6A6A6A] text-left">
-              {" "}
-              Araby AI
-            </div>
-            <div className="text-sm leading-[20px] md:leading-[40px] md:text-[40px] w-full">
-              {t("htmlhelpHeading")}
-            </div>
-            <input className="border-2 border-[#6900FF] rounded-lg  w-5/6 p-2  text-[12px] md:text-[16px] text-black bg-[#1c1c1ccc]" />
-            <button className="bg-gradient-to-r from-[#880CC2] to-[#6900FF] px-10 py-1  text-white text-[8px] md:text-[20px]">
+        <div className="border-gradient-help inline-flex items-center border-solid border-2 relative z-10 rounded-xl text-gray-100 text-xs md:text-lg gradient-border-3 flex-col justify-center gap-5 md:gap-5 p-1 basis-1/2 w-[250px] h-[330px] m-2">
+          <div className="bg-[#1c1c1ccc] w-full flex justify-center items-center flex-col gap-5 object-contain h-full">
+            <h3 className="text-sm md:text-[40px] leading-[20px] md:leading-[40px] text-[#6A6A6A] text-left"> Araby AI</h3>
+            <div className="text-sm leading-[20px] md:leading-[40px] md:text-[40px] w-full">{t("htmlhelpHeading")}</div>
+            <input className="border-2 border-[#6900FF] rounded-lg w-5/6 p-2 text-sm md:text-[16px] text-black bg-[#1c1c1ccc]" aria-label="User Input" />
+            <button className="bg-gradient-to-r from-[#880CC2] to-[#6900FF] px-10 py-1 text-white text-[8px] md:text-[20px]" aria-label="Enter Button">
               {t("htmlenter")}
             </button>
           </div>
         </div>
-      </div>
-      <div className=" flex items-center justify-center relative w-full h-full ">
-        <div className="absolute  w-full  mt-12">
-          <Image
-            className="m-auto hidden md:block"
-            src={pythonBackground}
-            alt="python image"
-            width={500}
-            height={500}
-          />
-        </div>
+      </article>
+
+      <div className="flex items-center justify-center relative w-full h-full ">
+        <Image className="m-auto hidden absolute md:block" src={pythonBackground} alt="Python Background Image" width={500} height={500} />
       </div>
 
-      <div className="flex container  flex-col-reverse md:flex-row items-center justify-center  relative  gap-10 md:gap-36 pb-20 ">
-        <Image
-          src={htmlCode}
-          alt="python image"
-          className=" md:w-1/3 w-1/2  object-cover z-10"
-        />
-        <ul className="list-disc marker:text-white text-left text-[12px] md:text-xl rtl:text-right ">
+      <section className="flex container flex-col-reverse md:flex-row items-center justify-center relative gap-10 md:gap-36 pb-20 ">
+        <Image src={htmlCode} alt="HTML Code Image" className="w-80 md:w-96 object-cover z-10" />
+        <ul className="list-disc marker:text-white text-left text-sm md:text-xl rtl:text-right ">
           <li>{t("htmlbullet1")}</li>
           <li>{t("htmlbullet2")}</li>
           <li>{t("htmlbullet3")}</li>
@@ -81,13 +68,9 @@ const HtmlCoding = () => {
           <li>{t("htmlbullet9")}</li>
           <li>{t("htmlbullet10")}</li>
         </ul>
-        <Image
-          src={pythonSide}
-          alt="python image"
-          className="scale-50 h-400px absolute top-36 right-0 invisible md:visible"
-        />
-      </div>
-    </div>
+        <Image src={pythonSide} alt="Python Side Image" className="scale-50 h-400px absolute top-36 right-0 invisible md:visible" />
+      </section>
+    </section>
   );
 };
 
